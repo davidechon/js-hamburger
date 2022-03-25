3bc57f0e2956222d3ccd9b659293094e28295d61
Davide Chon <57179442+davidechon@users.noreply.github.com>
2022-03-25 17:05:51 +0100

    update flowchart

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
-// flowchart TD
-//    A[INIZIO] --> B[nascondi il menu principale <1000px]
-//    B --> C[mostra click l'hamburger  menu]
-//    C --> D[apri l'hamburger menu]
-//    D --> E[chiudi l'hamburger menu]
-//    E --> P[FINE]
+//flowchart TD
+//A[INIZIO] --> B[selezionare la classe del menu]
+//B -->|open| C[selezionare la classe hamburger-menu]
+//C -->|close| D[assegna al click la funzione open>expand]
+//D -->|open>expand| E[seleziona l'hamburger-menu]
+//E -->|expand change| F[cambia stile display=block]
+//F -->|expand change| G[assegna al click la funzione close>expand]
+//G -->|close>expand| H[seleziona l'hamburger-menu]
+//H -->|expand change| I[cambia stile display=none]
+//I --> Z[FINE]
 
 
 let open = document.querySelector(".header-right > a");
